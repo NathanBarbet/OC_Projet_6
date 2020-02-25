6be4b7cbe4d46215b56ea6437df37bc37b92dfc8
NathanBarbet <nathan.barbet@hotmail.fr>
2020-02-25 10:04:00 +0100

    Fix issues Codacy

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -8,7 +8,7 @@ require dirname(__DIR__).'/vendor/autoload.php';
 // Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
 if (is_array($env = @include dirname(__DIR__).'/.env.local.php') && (!isset($env['APP_ENV']) || (filter_input(INPUT_SERVER, 'APP_ENV') ?? filter_input(INPUT_ENV, 'APP_ENV') ?? $env['APP_ENV']) === $env['APP_ENV'])) {
     foreach ($env as $k => $v) {
-        $_ENV[$k] = filter_input(INPUT_ENV, $k) ?? (filter_input(INPUT_SERVER, $k) && 0 !== strpos($k, 'HTTP_') ? filter_input(INPUT_SERVER, $k) : $v);
+        $k = filter_input(INPUT_ENV, $k) ?? (filter_input(INPUT_SERVER, $k) && 0 !== strpos($k, 'HTTP_') ? filter_input(INPUT_SERVER, $k) : $v);
     }
 } elseif (!class_exists(Dotenv::class)) {
     throw new RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
